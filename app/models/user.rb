63d773bb368e0c90ed59ab82993715cdc064aced
Eugene Khrantsov <eugene.kstu@gmail.com>
2012-08-24 02:38:13 +0400

    second

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable,, :confirmable :registerable, :validatable,
   # :timeoutable and :omniauthable  ,:recoverable,
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
          :rememberable, :trackable,
          :lockable
 
